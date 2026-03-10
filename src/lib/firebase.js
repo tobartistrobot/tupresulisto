910b2b2d321067998b0d0ea7d49a59b41e304bc1
Tob <tobartistrobot@gmail.com>
2026-03-10 01:15:00 +0100

    Security(auth): move firebase config to NEXT_PUBLIC env vars

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBzIxWGRtcmjMafcRud91WGJTnSpJhYx7c",
-    authDomain: "tupresulisto.firebaseapp.com",
-    projectId: "tupresulisto",
-    storageBucket: "tupresulisto.firebasestorage.app",
-    messagingSenderId: "755896919636",
-    appId: "1:755896919636:web:ab79e04b27f974ebf2afc1",
-    measurementId: "G-B7VXNVG8JJ"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
